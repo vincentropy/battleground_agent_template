1beb1ae278dedcacd0b9756ef7f932837a4253e0
phylyc <0d26ae7a@opayq.com>
2018-02-09 00:56:47 +0000

    adding comments

--- basic_agent/my_persistent_agent.py
+++ basic_agent/my_persistent_agent.py
@@ -17,10 +17,11 @@ class MyPersistentAgent(Agent):
         memory = self.get_memory(default=self.default_mem)
         # after initialization or if something made it forget:
         self.set_memory(memory)
+        # implementing temporal-difference learning:
         # if last turn was my turn
         if state["turn"] - 1 == self.my_turn_num:
             last_roll = state["last_roll"]
             delta = last_roll - memory["guess"]
-            # improve guess based on last roll data
+            # improve guess based on last roll data with size-step parameter 0.01
             memory["guess"] += delta * 0.01
             self.set_memory(memory)
