2eade368fbe14aaf7e20b0bf47250bd937ef8008
phylyc <0d26ae7a@opayq.com>
2018-05-14 17:32:55 +0100

    include function to flatten move_options into single list of options

--- arena_agent/my_persistent_arena_agent.py
+++ arena_agent/my_persistent_arena_agent.py
@@ -6,6 +6,55 @@ class MyPersistentAgent(Agent):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    @staticmethod
+    def move_options_to_list(move_options):
+        """
+        :param move_options: from get_move_options(gladiator_index)
+        :return: list of move options, from which an agent can directly select
+        """
+        options_list = []
+        option = []
+
+        def to_dict(list_of_tuples):
+            return {t[0]: t[1] for t in list_of_tuples}
+
+        def options_culling(arg):
+            if isinstance(arg, list):
+                for element in arg:
+                    options_culling(element)
+                    del option[-1]
+            elif isinstance(arg, dict):
+                # the dict is unordered, but we first need to cull the description of the option
+                # before nesting further. The dict has len == 2, so it's cheap.
+                for k, v in arg.items():
+                    if not isinstance(v, list):
+                        option.append((k, v))
+                for k, v in arg.items():
+                    if isinstance(v, list):
+                        if isinstance(v[0], dict):  # v is list of dicts
+                            options_culling(v)
+                        else:  # v is list of values
+                            # they keys are strings, and for the last list of options they are given as
+                            #   'values': [ ... ]
+                            # removing the plural 's' gives the singular form which appears in the move dict
+                            # ASSUMPTION: A KEY THAT ENDS ON 's' IS IN PLURAL FORM
+                            if k[-1] is 's':
+                                key = k[:-1]
+                            else:
+                                key = k
+                            for value in v:
+                                option.append((key, value))
+                                options_list.append(to_dict(option))
+                                del option[-1]
+            else:  # will never be reached
+                option.append(arg)
+                options_list.append(to_dict(option))
+                del option[-1]
+            return None
+
+        options_culling(move_options)
+        return options_list
+
     def move(self, state):
         """
         This agent can also play the bunnies game and the basic games.
@@ -17,30 +66,8 @@ class MyPersistentAgent(Agent):
         if state is not None:
             if "move_options" in state:
                 options = state["move_options"]
-                if isinstance(options, list):
-                    options = random.choice(options)
-                if "type" in options:
-                    move["type"] = options["type"]
-                else:
-                    move["type"] = options
-
-                has_targets = False
-                if isinstance(options, dict) and "targets" in options:
-                    options = random.choice(options["targets"])
-                    has_targets = True
-                if isinstance(options, dict) and "target" in options:
-                    move["target"] = options["target"]
-                elif has_targets:
-                    move["target"] = options
-
-                has_values = False
-                if isinstance(options, dict) and "values" in options:
-                    options = random.choice(options["values"])
-                    has_values = True
-                if isinstance(options, dict) and "value" in options:
-                    move["value"] = move["value"]
-                elif has_values:
-                    move["value"] = options
+                options_list = self.move_options_to_list(options)
+                move = random.choice(options_list)
             else:
                 # default values
                 move["name"] = "stay"
